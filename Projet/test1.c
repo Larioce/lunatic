f064e31487d8a6a545df16d8cb9550f259d0f7b6
Larioce <legrandpachedor@gmail.com>
2022-08-19 12:16:05 +0100

    Projet

---
+++ Projet/test1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main() {
+
+    printf("Entrez les caractères : ");
+    char  pa[1000];
+    scanf("%s",&pa);
+    printf("Entrez le ou les caractères dont vous souhaitez voir la repetition et la frequence : ");
+    char car[10];
+    scanf("%s",&car);
+
+    return 0;
+}
